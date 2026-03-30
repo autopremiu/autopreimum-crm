2cc58c01388b8ebc1690829c9df608ede6046639
auto premium <comercialautopremium@gmail.com>
2026-03-30 09:08:53 -0500

    se agrego  docker y agrego puerto 8080

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const { Pool } = require('pg');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, async () => {
   console.log("🚗 Auto Premium Service CRM");
